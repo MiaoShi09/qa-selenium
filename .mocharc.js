09e8880f2db35e6462fcf68124280a19f038a1d4
MiaoShi09 <miao@theoan.com>
2020-04-07 12:14:59 +0800

    add junit reporter

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = {
+	"reporter": ['mocha-junit-reporter'],
+    "reporterOptions": {
+        "mochaFile": "./test_reports/test"+Date.now()+".xml"
+    },
   "spec": [
     "./test/**.test.js"
     //"test/browse_mode.test.js"
   ],
-  timeout: 0,
+  "timeout": 0,
 };
